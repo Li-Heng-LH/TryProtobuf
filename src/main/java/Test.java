0e090968c0739f995fe80e7621222a993686644b
Li Heng <liheng6567@gmail.com>
2019-10-04 00:07:15 +0800

    Make use of generated proto java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,17 @@ import com.me.TryProtobuf.AddressBookProtos;
 
 public class Test {
     public static void main(String[] args) {
+
+        AddressBookProtos.Person person = AddressBookProtos.Person.newBuilder()
+                .setId(1234)
+                .setName("John Doe")
+                .setEmail("jdoe@example.com")
+                .addPhones(
+                        AddressBookProtos.Person.PhoneNumber.newBuilder()
+                                .setNumber("555-4321")
+                                .setType(AddressBookProtos.Person.PhoneType.HOME))
+                .build();
+
+        System.out.println(person);
     }
 }
